2a2916f61d458e08b66cacbd523178262c71b087
PLUG <ppuwdon@gmail.com>
2025-01-31 00:22:07 +0700

    update count score

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { useState,useRef,useEffect } from 'react'
+import { useState,useRef,useCallback, useEffect } from 'react'
 import './App.css'
 
 function App() {
+    const [countscore,setcountscore] = useState(0);
+    const [counthiscore,setcounthiscore] = useState(0);
     const playgame = useRef(true);
     const plane = useRef(null);
     const road = useRef(null);
@@ -9,17 +11,19 @@ function App() {
     const gamebox = useRef(null);
     const boxtown = useRef(null);
     const arrtown = useRef([]);
+    const death = useRef(false);
+    const speedlevel = useRef(2);
 
     document.addEventListener("keydown",(event) => {
         if (event.key == " ") {
+            let speedroad = 0;
+            let speedroad2 = 0;
+            let polleftgamebox = 0;
+            let polrightroad = 0;
+            let polrightroad2 = 0;
+
             if (playgame.current) {
                 playgame.current = false
-                let speedroad = 0;
-                let speedroad2 = 0;
-                let speedlevel = 2;
-                let polleftgamebox = 0;
-                let polrightroad = 0;
-                let polrightroad2 = 0;
 
                 if (road2.current) {
                     speedroad2 = road2.current.offsetWidth;
@@ -54,12 +58,32 @@ function App() {
                         });
                     }
 
-                    speedroad -= speedlevel;
-                    speedroad2 -= speedlevel;
+                    speedroad -= speedlevel.current;
+                    speedroad2 -= speedlevel.current;
 
                     arrtown.current.forEach(el => {
-                        el.pol -= speedlevel;
+                        el.pol -= speedlevel.current;
                     });
+
+                    arrtown.current.forEach(el => {
+                        const polplane = plane.current.getBoundingClientRect();
+                        const polarrtown = el.e.getBoundingClientRect();
+
+                        if (polplane.top < polarrtown.bottom &&
+                            polplane.bottom > polarrtown.top &&
+                            polplane.left < polarrtown.right &&
+                            polplane.right > polarrtown.left ||
+                            polplane.top < polarrtown.bottom &&
+                            polplane.bottom > polarrtown.top &&
+                            polplane.left < polarrtown.right &&
+                            polplane.right > polarrtown.left) {
+                                death.current = true;
+                                speedlevel.current = 0;
+                                plane.current.classList.add("addanimationpause");
+                        }
+                    });
+
+                    return () => clearInterval(time);
                 },0);
 
                 let time2 = setInterval(() => {
@@ -81,13 +105,32 @@ function App() {
                         boxtown.current.appendChild(createimg);
                         arrtown.current.push({e:createimg,pol:droplength[randomdroplength]});
                     }
+
+                    return () => clearInterval(time2);
                 },1200);
 
-                return () => clearInterval(time);
+                let time3 = setInterval(() => {
+                    if (!death.current) {
+                        setcountscore(prev => prev + 1);
+                    }
+
+                    return () => clearInterval(time3);
+                },100);
             }
             else {
                 if (plane.current) {
-                    plane.current.classList.add("addplaneani");
+                    if (!death.current) {
+                        plane.current.classList.add("addplaneani");
+                    }
+                    else {
+                        speedlevel.current = 2;
+                        death.current = false;
+                        arrtown.current = [];
+                        plane.current.classList.remove("addplaneani");
+                        plane.current.classList.remove("addanimationpause");
+                        boxtown.current.innerHTML = "";
+                        setcountscore(prev => 0);
+                    }
                 }
             }
         }
@@ -99,14 +142,26 @@ function App() {
         }
     }
 
+    useEffect(() => {
+        if (countscore > 0) {
+            if (countscore >= counthiscore) {
+                setcounthiscore(prev => countscore);
+            }
+        }
+    },[countscore]);
+
     return (
       <div ref={gamebox} className="boxgame">
-          <img ref={plane} onAnimationEnd={aniend} className="plane" src="./plane.png" alt="" />
-          <img ref={road} className="road" src="./road.png" alt="" />
-          <img ref={road2} className="road" src="./road.png" alt="" />
-          <div ref={boxtown} className="boxtown"></div>
+        <div className="displayscore">
+            <h2 className="hiscore">HI {counthiscore}</h2>
+            <h2 className="score">{countscore}</h2>
+        </div>
+        <img ref={plane} onAnimationEnd={aniend} className="plane" src="./plane.png" alt="" />
+        <img ref={road} className="road" src="./road.png" alt="" />
+        <img ref={road2} className="road" src="./road.png" alt="" />
+        <div ref={boxtown} className="boxtown"></div>
       </div>
-    )
+    );
 }
 
 export default App;
